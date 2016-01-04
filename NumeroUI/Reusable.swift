6e2141c4ef95e9413bab45fee1556b24db0317ad
Aron Cedercrantz <cedercra@spotify.com>
2016-01-04 14:43:13 -0500

    Add Reusable

--- NumeroUI/Reusable.swift
+++ NumeroUI/Reusable.swift
@@ -1,11 +1,14 @@
-//
-//  Reusable.swift
-//  NumeroUI
-//
-//  Created by Aron Cedercrantz on 04/01/16.
-//  Copyright © 2016 Numero Labs. All rights reserved.
-//
+// Copyright © 2016 Aron Cedercrantz. All rights reserved.
 
-import Foundation
+import UIKit
 
+/// The `Reusable` protocol describes an entity which can be re-used. For example a cell in a table view.
+public protocol Reusable {
 
+    /// A string that identifies the purpose of the entity. (read-only)
+    var reuseIdentifier: NSString { get }
+
+    /// Performs any clean up necessary to prepare the entity for use again.
+    func prepareForReuse()
+
+}
