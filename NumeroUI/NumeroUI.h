85c02266d6450d2559f05c4a8fb55f6ec830d5e5
Aron Cedercrantz <cedercra@spotify.com>
2016-01-04 14:20:35 -0500

    Add initial Xcode project

---
+++ NumeroUI/NumeroUI.h
@@ -0,0 +1,19 @@
+//
+//  NumeroUI.h
+//  NumeroUI
+//
+//  Created by Aron Cedercrantz on 04/01/16.
+//  Copyright © 2016 Numero Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NumeroUI.
+FOUNDATION_EXPORT double NumeroUIVersionNumber;
+
+//! Project version string for NumeroUI.
+FOUNDATION_EXPORT const unsigned char NumeroUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NumeroUI/PublicHeader.h>
+
+
---
+++ NumeroUI/Reusable.swift
@@ -0,0 +1,11 @@
+//
+//  Reusable.swift
+//  NumeroUI
+//
+//  Created by Aron Cedercrantz on 04/01/16.
+//  Copyright © 2016 Numero Labs. All rights reserved.
+//
+
+import Foundation
+
+
